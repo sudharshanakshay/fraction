6eb0e1bf075d1525e8e436127a056d1eca5f7803
sudharshan <sudharshan6acharya@gmail.com>
2023-07-27 15:59:06 +0000

    MMMMEEEEd

--- lib/screens/expense/view_expense.dart
+++ lib/screens/expense/view_expense.dart
@@ -5,6 +5,7 @@ import 'dart:math';
 import 'package:provider/provider.dart';
 import '../../services/expense/expense.services.dart';
 import '../../services/group/group.services.dart';
+import 'package:intl/intl.dart';
 
 class ViewExpenseLayout extends StatefulWidget {
   const ViewExpenseLayout({Key? key}) : super(key: key);
@@ -22,30 +23,27 @@ class ViewExpenseLayoutState extends State<ViewExpenseLayout> {
           stream: groupServiceState.getMemberDetails(),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        width: 2,
-                        color: getRandomColor(),
-                      ),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    padding: const EdgeInsets.all(10),
-                    child: GridView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: snapshot.data?.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return AccountPallet(
-                            streamSnapshot: snapshot, index: index);
-                      },
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2, childAspectRatio: (1 / .4)),
-                    ),
-                  ));
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                    color: getRandomColor(),
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: const EdgeInsets.all(10),
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: snapshot.data?.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return AccountPallet(
+                        streamSnapshot: snapshot, index: index);
+                  },
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, childAspectRatio: (1 / .4)),
+                ),
+              );
             } else {
               return Container();
             }
@@ -56,41 +54,46 @@ class ViewExpenseLayoutState extends State<ViewExpenseLayout> {
   @override
   Widget build(BuildContext context) {
     return Consumer<ExpenseService>(builder: (context, expenseServiceState, _) {
-      return StreamBuilder(
-          stream: expenseServiceState.getExpenseCollection(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return const Text('Loading ...');
-            }
-            return SingleChildScrollView(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                  accountDetailWidget(),
-                  Text(expenseServiceState.currentUserName),
-                  ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      padding: const EdgeInsets.all(8),
-                      itemCount: snapshot.data?.docs.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return ExpensePallet(
-                            description: snapshot.data!.docs[index]
-                                ['description'],
-                            cost: snapshot.data!.docs[index]['cost'],
-                            currentUserEmail:
-                                expenseServiceState.currentUserEmail,
-                            currentUserName:
-                                expenseServiceState.currentUserName,
-                            memberEmail: snapshot.data!.docs[index]
-                                ['emailAddress'],
-                            time: snapshot.data!.docs[index]['timeStamp']);
-                      }),
-                  const SizedBox(
-                    height: 70,
-                  )
-                ]));
-          });
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: StreamBuilder(
+            stream: expenseServiceState.getExpenseCollection(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Text('Loading ...');
+              }
+              return SingleChildScrollView(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                    accountDetailWidget(),
+                    Text(
+                        DateFormat.MMMMEEEEd().format(
+                            snapshot.data!.docs[0]['timeStamp'].toDate()),
+                        style: const TextStyle(fontSize: 20)),
+                    ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: snapshot.data?.docs.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ExpensePallet(
+                              description: snapshot.data!.docs[index]
+                                  ['description'],
+                              cost: snapshot.data!.docs[index]['cost'],
+                              currentUserEmail:
+                                  expenseServiceState.currentUserEmail,
+                              currentUserName:
+                                  expenseServiceState.currentUserName,
+                              memberEmail: snapshot.data!.docs[index]
+                                  ['emailAddress'],
+                              time: snapshot.data!.docs[index]['timeStamp']);
+                        }),
+                    const SizedBox(
+                      height: 70,
+                    )
+                  ]));
+            }),
+      );
     });
   }
 }
