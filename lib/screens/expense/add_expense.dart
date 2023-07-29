9ab901866679363c702ada0b5d656f33ed30cd6a
sudharshan <sudharshan6acharya@gmail.com>
2023-07-29 14:55:41 +0000

    added validation for add expense

--- lib/screens/expense/add_expense.dart
+++ lib/screens/expense/add_expense.dart
@@ -22,6 +22,22 @@ class _AddExpenseLayoutState extends State<AddExpenseLayout> {
   final _descriptionTextController = TextEditingController();
   final _costTextController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
+  validateNotEmptyDesc(value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter some text';
+    }
+    return null;
+  }
+
+  validateNotEmptyCost(value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter cost';
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,42 +47,49 @@ class _AddExpenseLayoutState extends State<AddExpenseLayout> {
       ),
       body: Consumer<ExpenseService>(
         builder: (context, expenseServiceState, _) => Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                FractionallySizedBox(
-                    widthFactor: 0.7,
-                    //heightFactor: 0.2,
-                    child: TextField(
-                        controller: _descriptionTextController,
-                        decoration: const InputDecoration(
-                          label: Text('Item Name'),
-                        ))),
-                const SizedBox(height: 10),
-                FractionallySizedBox(
-                    widthFactor: 0.7,
-                    //heightFactor: 0.2,
-                    child: TextField(
-                        controller: _costTextController,
-                        keyboardType: TextInputType.number,
-                        decoration: const InputDecoration(
-                          label: Text('Item Cost'),
-                        ))),
-                const SizedBox(height: 10),
-                FilledButton(
-                    onPressed: () async {
-                      const snakBar =
-                          SnackBar(content: Text('adding expense ...'));
-                      ScaffoldMessenger.of(context).showSnackBar(snakBar);
-                      expenseServiceState
-                          .addExpense(
-                              description: _descriptionTextController.text,
-                              cost: _costTextController.text)
-                          .whenComplete(() => Navigator.pop(context));
-                    },
-                    child: const Text('Save')),
-              ]),
+          child: Form(
+            key: _formKey,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  FractionallySizedBox(
+                      widthFactor: 0.7,
+                      //heightFactor: 0.2,
+                      child: TextFormField(
+                          validator: (value) => validateNotEmptyDesc(value),
+                          controller: _descriptionTextController,
+                          decoration: const InputDecoration(
+                            label: Text('Item Name'),
+                          ))),
+                  const SizedBox(height: 10),
+                  FractionallySizedBox(
+                      widthFactor: 0.7,
+                      //heightFactor: 0.2,
+                      child: TextFormField(
+                          validator: (value) => validateNotEmptyCost(value),
+                          controller: _costTextController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            label: Text('Item Cost'),
+                          ))),
+                  const SizedBox(height: 10),
+                  FilledButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          const snakBar =
+                              SnackBar(content: Text('adding expense ...'));
+                          ScaffoldMessenger.of(context).showSnackBar(snakBar);
+                          expenseServiceState
+                              .addExpense(
+                                  description: _descriptionTextController.text,
+                                  cost: _costTextController.text)
+                              .whenComplete(() => Navigator.pop(context));
+                        }
+                      },
+                      child: const Text('Save')),
+                ]),
+          ),
         ),
       ),
     );
