3727aa2c635a3a8ea8ea909dfd3e66d109796e3d
sudharshan <sudharshan6acharya@gmail.com>
2023-07-29 11:37:55 +0000

    match theme color

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Fraction',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: AppColors().themeColor),
           useMaterial3: true,
--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -4,5 +4,5 @@ class AppColors {
   Color get themeColor => const Color.fromRGBO(36, 110, 233, 0);
   Color get textInputColor => Colors.blueAccent;
   Color get tags => Colors.blue.shade100;
-  Color get appDrawerHeaderBackgroudColor => Colors.blue.shade400;
+  Color get appDrawerHeaderBackgroudColor => Color.fromARGB(255, 158, 177, 239);
 }
