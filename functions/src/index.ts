b53cd0199cd7672da88397bca007fc3dd53fb815
sudharshan <sudharshan6acharya@gmail.com>
2023-09-07 10:21:53 +0000

    tesing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,14 +8,22 @@
  */
 
 // import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-const {onRequest} = require("firebase-functions/v2/https");
+// import * as logger from "firebase-functions/logger";
+// import {onRequest} from "firebase-functions/v2/https";
+import {onDocumentWritten} from "firebase-functions/v2/firestore";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request:any, response:any) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// export const helloWorld = onRequest((request:any, response:any) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
 
+// trigger events expense is added.
+
+exports.expenseTrigger =
+onDocumentWritten("expense/{groupName}/{groupInstance}/{expenseDocId}",
+  () => {
+    return "hello, from expense trigger";
+  });
